b3b301aa8a4d50be3f4fdce3016ff1ad8e7f7e87
CzermakP <45134641+CzermakP@users.noreply.github.com>
2018-11-17 21:36:35 -0500

    Update Box.java
    Updated Lab3 with comments

--- Lab3/src/Box.java
+++ Lab3/src/Box.java
@@ -2,50 +2,50 @@ import java.util.Scanner;
 
 public class Box {
 
-	public double length, width, height;
+	public double length, width, height; //declaring data members
 
-	public Scanner input;
+	public Scanner input; //declaring scanner input
 
-	Box() {
+	Box() { //default constructor
 		length = 1;
 		width = 1;
 		height = 1;
 		input = new Scanner(System.in);
 	}
 
-	Box(double aLength, double aWidth, double aHeight) {
+	Box(double aLength, double aWidth, double aHeight) { //parameterized constructor
 		length = aLength;
 		width = aWidth;
 		height = aHeight;
 		input = new Scanner(System.in);
 	}
 
-	Box(Box aBox) {
+	Box(Box aBox) { //copy constructor
 		length = aBox.length;
 		width = aBox.width;
 		height = aBox.height;
 	}
 
-	public void inputLength() {
+	public void inputLength() { //allows user to input length as a type double 
 		System.out.print("Enter Length: ");
 		length = input.nextDouble();
 	}
 
-	public void inputWidth() {
+	public void inputWidth() { //allows user to input width as a type double 
 		System.out.print("Enter Width: ");
 		width = input.nextDouble();
 	}
 
-	public void inputHeight() {
+	public void inputHeight() { //allows user to input height as type double
 		System.out.print("Enter Height: ");
 		height = input.nextDouble();
 	}
 
-	public void displayDimensions() {
+	public void displayDimensions() { //method to display box dimensions
 		System.out.print(length + " X " + width + " X " + height);
 	}
 
-	public double calcVolume() {
+	public double calcVolume() { //method to calculate box volume
 		return length * width * height;
 
 	}
